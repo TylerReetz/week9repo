ddab7a1cc209c5231ee18dce789b43356c0303a9
TylerReetz <reetz218@gmail.com>
2024-02-10 02:39:37 -0800

    week 5 coding assignment

---
+++ Week 5 Coding Project/src/SpacedLogger.java
@@ -0,0 +1,21 @@
+
+public class SpacedLogger implements Logger {
+	
+	public void log(String message) {
+		for (char c : message.toCharArray()) {
+            System.out.print(c + " ");
+        }
+        System.out.println();
+
+	}
+
+	public void error(String message) {
+		System.out.print("ERROR: ");
+        for (char c : message.toCharArray()) {
+            System.out.print(c + " ");
+        }
+        System.out.println();
+
+	}
+
+}
